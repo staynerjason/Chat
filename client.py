7a349291e5ba4e4fae4e42b53d311e81ea6174e9
staynerjason <62315666+staynerjason@users.noreply.github.com>
2022-02-04 10:28:08 -0800

    Refactor and added Docstrings

--- client.py
+++ client.py
@@ -1,11 +1,14 @@
-import socket
-import threading
 import selectors
+import socket
 import sys
+import threading
 
 
 class Client:
+    """This class acts as the cleint object handling the client side of the connection."""
+   
     def __init__(self, nickname: str):
+        """Initializes the attributes needed to host the client instance."""
         self.FORMAT: str = "utf-8"
         self.DISCONNECT_MSG: str = "!DISCONNECT"
         self.HEADER:int = 64
@@ -21,25 +24,28 @@ class Client:
         self.verify_nickname(nickname)
 
     def verify_nickname(self, nickname: str):
+        """this method handles the return code from the server verifying a good nickname."""
         self.client.send(nickname.encode(self.FORMAT))
         recv = int(self.client.recv(1024).decode(self.FORMAT).strip())
         if recv is self.ACCEPTED:self.main()
         if recv == self.REJECTED:
             raise Exception(f"Nickname {nickname} already in use, please try again...")
         
-              
     def recieve(self, bytes: int = 2046):
+        """this method listens for broadcast from the server and returns them to the client."""
         recv = self.client.recv(bytes).decode(self.FORMAT)
         if recv:print(recv)
             
     def msg_len(self, msg: str):
+        """this method returns the length of the in bytes for the HEADER message."""
         message = msg.encode(self.FORMAT)
         msg_length = len(message)
         send_length = str(msg_length).encode((self.FORMAT))
         send_length += b" " * (self.HEADER - len(send_length))
         return send_length
-
+        
     def get_message(self):
+        """This method waits for the user to input a message, then passes the string to the handle_message method."""
         sel = selectors.DefaultSelector()
         sel.register(sys.stdin, selectors.EVENT_READ)
         sys.stdout.flush()
@@ -47,6 +53,7 @@ class Client:
         if pairs:self.handle_message()
 
     def handle_message(self):
+        """this method takes the user input string and sends it to the send_msg method."""
         msg = sys.stdin.readline().strip()
         sys.stdout.write("\033[1A\033[K")
         if msg:
@@ -54,37 +61,37 @@ class Client:
                 self.disconnect()
             self.send_msg(msg)
 
+    def send_msg(self, message: str):
+        """this method takes the user input string, sends the length in a header msg, then sends the user string to the server."""
+        self.client.send(self.msg_len(message))
+        self.client.send(message.encode(self.FORMAT))
+    
     def disconnect(self):
-        self.RUNNING = False
+        """this method handlers the disconnection of the client from the server and closes the cleint program."""
+        self.running = False
         self.send_msg(self.DISCONNECT_MSG)
         print("[USER] Disconnected...")
         sys.exit()
-    
-    def send_msg(self, message: str):
-        self.client.send(self.msg_len(message))
-        self.client.send(message.encode(self.FORMAT))
 
-    def main(self):
-        self.RUNNING = True
+    def thread(self, method):
+        """This method instanciates a daemon thread for the passed method"""
         TIMEOUT: float = 0.5
         stop_event = threading.Event()
         thread_lock = threading.Lock()
-        while self.RUNNING:
-            recv = threading.Thread(target=self.recieve, daemon=True)
-            with thread_lock:
-                recv.start()
-                recv.join(timeout=TIMEOUT)
-                stop_event.set()
+        thread = threading.Thread(target=method, daemon=True)
+        with thread_lock:
+            thread.start()
+            thread.join(timeout=TIMEOUT)
+            stop_event.set()
 
-            msg = threading.Thread(target=self.get_message, daemon=True)
-            with thread_lock:
-                msg.start()
-                msg.join(timeout=TIMEOUT)
-                stop_event.set()
+    def main(self):
+        """this method acts as the main loop for the client, pass the get_message, and recieve method to the thread method."""
+        self.running = True
+        while self.running:
+            self.thread(self.get_message)
+            self.thread(self.recieve)
+
         
-        
-
-
 if __name__ == "__main__":
     client = Client(input("What is your Nickname..."))
     
--- server.py
+++ server.py
@@ -3,7 +3,9 @@ import threading
 
 
 class Server:
+    """This class handles the conncection of clients, broadcasting of client messeges and handling the nickname namespace."""
     def __init__(self):
+        """Initialize attributes needed for the server"""
         self.PORT: int = 5050
         self.SERVER: str = socket.gethostbyname(socket.gethostname())
         self.ADDR: tuple = (self.SERVER, self.PORT)
@@ -21,6 +23,8 @@ class Server:
         self.clients_lock = threading.Lock()
 
     def handle_client(self, conn, nickname):
+        """this method takes in a client thead and handles the incoming messages,
+        as well as broadcasting the messages to all the other clients."""
         try:
             connected = True
             conn.send(
@@ -45,19 +49,24 @@ class Server:
             self.broadcast("SERVER", f"{nickname} Disconnected...")
 
     def broadcast(self, nickname, msg):
+        """this method takes in a client nickname and the message, 
+        then sends the messages to all the client threads in the client set."""
         with self.clients_lock:
             for c in self.clients:
                 c.sendall(f"[{nickname}] {msg}".encode(self.FORMAT))
 
     def bad_nickname(self, nickname):
+        """this method verify that the nickname of the new connection is not in the nicknames set already.
+        If the nickname is not in the set, then the nick name will be added to the set."""
         if nickname in self.nicknames:
             return True
         else:self.nicknames.add(nickname)
 
     def start(self):
+        """this method acts as the server main loop, waiting for incomming connections, 
+        then handling the initialzation of the client as a new thread and passing the client to the handle client method"""
         print(f"[STARTING] server is starting @ {self.SERVER}")
         self.server.listen()
-        
         while True:
             conn, _ = self.server.accept()
             nickname = conn.recv(self.HEADER).decode(self.FORMAT).title()
@@ -74,11 +83,14 @@ class Server:
             self.active_connections = threading.activeCount() - 1
             print(f"[ACTIVE CONNECTIONS] {self.active_connections}")
 
+    def stop(self):    
+        print("\033[1A")
+        print("[STOPPING] server stopped...goodbye")
+
 
 if __name__ == "__main__":
     try:
         server = Server()
         server.start()
     except KeyboardInterrupt:
-        print("\033[1A")
-        print("[STOPPING] server stopped...goodbye")
+        server.stop()
